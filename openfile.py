6f8209b5e88f1ac9976a61d456c131d9d93b28fd
lanqilu <885240677@qq.com>
2023-02-23 12:32:40 +0800

    🦄 refactor: 优化文件路径处理

--- openfile.py
+++ openfile.py
@@ -6,25 +6,24 @@ from urllib.parse import unquote
 from tkinter import messagebox
 
 if len(sys.argv) < 2:
+    messagebox.showinfo("Open File", "未指定文件路径")
     sys.exit(1)
 
 file_path = sys.argv[1].encode('utf-8')
 # 将 url 字符串进行解码
 file_path = unquote(file_path)
 
-# 去除字符串第一个 : 前的所有字符
-# 同一将前缀改成 open: 
-# 注意该段代码会导致执行 py openfile.py C:\Users\ 这种命令出错
-# 需要添加前缀例如 py openfile.py openfile:\\C:\Users\
+# 去除字符串第一个 : 前的所有字符,并将前缀同一改成 open: 
 file_path = "open:" + file_path.split(':',1)[1]
 
-# 去除前缀 open: open:/ open:// open:\ open:\\
+# 去除前缀 open:、open:/、open://、open:\、open:\\
 regex = r"open:(?://|/|\\\\|\\)?"
 file_path = re.sub(regex, "", file_path)
-# 将文件路径 / 统一替换成 \
-file_path = file_path.replace('/', '\\')
-# 踩坑点 这里 " 会导致文件路径识别错误
-file_path = file_path.strip("'\"\\")
+# 去除路径前后的（'、"、\、‘、’、“、”）会导致文件路径识别错误
+file_path = file_path.strip("'\"\\‘’“”")
+# 规范化路径
+file_path = os.path.normpath(file_path)
+
 
 # debugger
 # print(sys.argv[1])
@@ -41,5 +40,5 @@ try:
 except FileNotFoundError:
     # 文件不存在，打印错误信息并退出程序
     # 弹出错误窗口
-    messagebox.showerror("Open File", file_path + "\n文件或文件夹不存在 \nThe file or folder does not exist")
+    messagebox.showinfo("Open File", file_path + "\n文件或文件夹不存在")
     sys.exit(1)
